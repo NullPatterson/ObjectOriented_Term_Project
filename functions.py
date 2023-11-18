b46f2eafe7d30ee29184cd1e4b1ddc38bd5e3aaf
Nol Patterson <nol.patterson1683@gmail.com>
2023-11-18 17:08:53 -0600

    Added print_all_contacts() to functions.py

--- functions.py
+++ functions.py
@@ -2,10 +2,9 @@
 # Date: 11/20/2023
 # Purpose: Define functions used in driver.py for the CSC2310-001 Term Project
 
-
 # Function Name: menu_validation()
 # Purpose: Check a user input against a range of acceptable values
-# Paramaters: choice, the menu choice made by the user; will be in the format of "x" where x is an integer
+# Parameters: choice, the menu choice made by the user; will be in the format of "x" where x is an integer
 #             minval, the minimum integer choice could be
 #             maxval, the maximum integer choice could be
 # Returns: int(choice); after it is validated that  minval <= int(choice) <= maxval
@@ -19,3 +18,9 @@ def menu_validation(choice, minval, maxval):
         choice = input()
         menu_validation(choice, minval, maxval)
     return int(choice)
+
+def print_all_contacts(*contacts):
+    print("UserID\tLast, First Name")
+    for contact in contacts:
+        print("{0}\t{1}, {2}".format(contact.user_id, contact.last_name, contact.first_name))
+    print()
