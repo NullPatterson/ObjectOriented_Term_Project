35c7cf3a627b7249866759b958e31e6221954176
Nol Patterson <nol.patterson1683@gmail.com>
2023-11-18 15:37:18 -0600

    Adjusted whitespace in driver.py

--- driver.py
+++ driver.py
@@ -15,39 +15,33 @@ eventListIterator = 0
 # Reading data from contacts.json
 with open("contacts.json") as contact_file:
     contact_data = json.load(contact_file)
-
 # Creating a list of contacts
 listContacts = []
 for contact in contact_data:
     newContact = Contact(contact['FirstName'], contact['LastName'], contact['UID'], contact['EmailAddress'],
                          contact['Dept'], contact['Title'], contact['Phone'], contact['Building'], contact['POBox'])
-
     listContacts.append(newContact)
     # Debug Statements to check if Events were read in properly
     # print("{0}: {1}".format(listContacts[contactListIterator].user_id, listContacts[contactListIterator].first_name))
     # contactListIterator = contactListIterator + 1
 # contactListIterator = 0  # Used to reset eventListIterator if used for debugging
-
 contact_file.close()
 
-# Debug Statement to see if all contact info is able to be outputted
+# Debug Statement to see if all contact info is able to be output
 # listContacts[0].contact_info()
 
 # Reading data from events.json
 with open("events.json") as event_file:
     event_data = json.load(event_file)
-
 # Creating a list of events
 listEvents = []
 for event in event_data['university_events']:
     newEvent = Event(event['Name'], event['UID'], event['Date'],
                      event['StartTime'], event['Location'], event['Duration'])
-
     listEvents.append(newEvent)
     # Debug Statements to check if Events were read in properly
     # print("{0}: {1}".format(listEvents[eventListIterator].name, listEvents[eventListIterator].location))
     # eventListIterator = eventListIterator + 1
 # eventListIterator = 0  # Used to reset eventListIterator if used for debugging
-
 event_file.close()
 
